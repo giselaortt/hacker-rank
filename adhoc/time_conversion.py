9065a624b7f889c53e49779677c8fad7f065f779
gisela <giselaortt@gmail.com>
2022-07-05 12:34:39 -0300

    simple problem for preparation for the intreview

---
+++ adhoc/time_conversion.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    if( s[-2] == 'P' ):
+        hr = int(s[0:2])+12
+        print(hr)
+        return str(hr)+s[2:-2]
+    else:
+        return s[0:-2]
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
