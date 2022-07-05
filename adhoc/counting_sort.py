305231f759fbdc44d4007b7a8c8acc89dfd0f8e9
Gi Ortt <ms.giselaortt@gmail.com>
2022-07-05 13:40:43 -0300

    simple problem for preparation for the intreview

---
+++ adhoc/counting_sort.py
@@ -0,0 +1,39 @@
+#https://www.hackerrank.com/challenges/one-week-preparation-kit-countingsort1/problem?h_l=interview&isFullScreen=true&playlist_slugs%5B%5D%5B%5D=preparation-kits&playlist_slugs%5B%5D%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D%5B%5D=one-week-day-two&h_r=next-challenge&h_v=zen
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    counted = [0]*100
+    for element in arr:
+        counted[element] = counted[element]+1
+
+    return counted
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
