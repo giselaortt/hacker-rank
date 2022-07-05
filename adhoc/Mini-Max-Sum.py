2f3923459001f2076b4bdb839e53d6c7174c0440
Gi Ortt <ms.giselaortt@gmail.com>
2022-07-05 12:26:34 -0300

    simple problem for preparation for the intreview

---
+++ adhoc/Mini-Max-Sum.py
@@ -0,0 +1,25 @@
+#  https://www.hackerrank.com/challenges/one-week-preparation-kit-mini-max-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one&h_r=next-challenge&h_v=zen
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    print(sum(arr)-max(arr), sum(arr) - min(arr))
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
