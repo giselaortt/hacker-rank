4f3b6240a5063113e603c24cfcb2637aed287985
gisela ortt <ms.giselaortt@gmail.com>
2022-07-07 20:26:08 -0300

    one more problem

---
+++ adhoc/Recursive_Digit_sum.py
@@ -0,0 +1,34 @@
+#https://www.hackerrank.com/challenges/one-week-preparation-kit-recursive-digit-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-f
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def superDigit(n, k):
+    n = str(n)
+    number = ''.join(n*k)
+    while(len(number) > 1):
+        number = str(sum( [int(value) for value in number] ))
+        
+    return number
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = first_multiple_input[0]
+
+    k = int(first_multiple_input[1])
+
+    result = superDigit(n, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
