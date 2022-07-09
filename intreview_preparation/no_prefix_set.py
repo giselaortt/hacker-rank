a338a15b552d98b1ca378c9d313a53fd4543a4b7
gisela ortt <ms.giselaortt@gmail.com>
2022-07-09 19:48:01 -0300

    error fixed on the process to generate the prefixes

--- intreview_preparation/no_prefix_set.py
+++ intreview_preparation/no_prefix_set.py
@@ -10,13 +10,16 @@ import re
 import sys
 
 
+'''
 def generateAllPrefixes( word ):
     answer = set()
-    for i in range( len(word) - 1 ):
+    for i in range( len(word) ):
         answer.add( word[ : i ] )
 
     return answer
-
+'''
+##  The big question here is, how to generate only the minimal amount of prefixes that i will need ?
+## TML with this solution with 2 hashtables on 7 testcases.
 
 if __name__ == '__main__':
     n = int(input().strip())
@@ -30,13 +33,18 @@ if __name__ == '__main__':
         if( newWord in words or newWord in prefixes ):
             answer = "BAD SET\n"+newWord
             break
+        
         else:
             words.add( newWord )
+            '''
             newPrefixes = generateAllPrefixes( newWord )
+
             if( newPrefixes & words ):
                 answer = "BAD SET\n"+newWord
                 break
+
             prefixes = prefixes.union( newPrefixes ) 
+            '''
 
     print(answer)
 
